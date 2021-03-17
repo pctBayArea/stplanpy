ac971d464bb09d841e741f361b1c07e8badb17d9
Arnout Boelens <ampboelens@gmail.com>
2021-03-17 09:57:34 -0700

    Option to change column names

--- stplanpy/od_lines.py
+++ stplanpy/od_lines.py
@@ -5,13 +5,13 @@ import pandas_flavor as pf
 from shapely.geometry import LineString
 
 @pf.register_dataframe_method
-def od_lines(fd: pd.DataFrame, centroids: pd.DataFrame) -> pd.DataFrame:
+def od_lines(fd: pd.DataFrame, centroids: pd.DataFrame, orig="orig_taz", dest="dest_taz") -> pd.DataFrame:
     
-    def f(*x):
+    def lines(*x):
         p0 = centroids.loc[x[0]]
         p1 = centroids.loc[x[1]]
         return LineString([p0, p1])
     
-    df = fd[["orig_taz", "dest_taz"]].apply(lambda x: f(*x), axis=1)
+    df = fd[[orig, dest]].apply(lambda x: lines(*x), axis=1)
 
     return df
